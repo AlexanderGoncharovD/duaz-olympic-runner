086a1a239db777f921e199c9ecf31385208cb262
Alexander Goncharov <goncharov54338@gmail.com>
2019-05-20 01:16:55 +0300

    Location generation update

---
+++ Duaz/Assets/scripts/CloudGeneration.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudGeneration : MonoBehaviour {
+
+    public Sprite[] Clouds; // Спрайты облаков
+
+    [Header("Settings")]
+    public Transform Chunk; // отправная точка спавна
+    public Vector2 LocationSize = new Vector2(30, 5); // длина локации
+    public Vector2 DenyMinMaxX = new Vector2(2.0f, 5.0f); // плотность спавна облаков
+    public Vector2 DenyMinMaxY = new Vector2(2.0f, 5.0f); // плотность спавна облаков
+
+    [Header("Other")]
+    public GameObject prefabEmptySprite; // пустая болванка для спрайта
+    public Transform ParentDecoration; // Родительский объект для декораций
+    GameObject LayersGO;  // группировка декорация по номеру слоя
+
+    void Start () {
+        LayersGO = new GameObject();
+        LayersGO.name = "layer";
+        LayersGO.tag = "layer";
+        LayersGO.transform.parent = ParentDecoration;
+
+        CloudGenerationGO();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void CloudGenerationGO ()
+    {
+        GameObject newCloud;
+
+        for(float i = 0; i < LocationSize.x; i += Random.Range(DenyMinMaxX.x, DenyMinMaxX.y))
+        {
+            newCloud = Instantiate(prefabEmptySprite, new Vector3(Chunk.position.x + i, Chunk.position.y
+                + Random.Range(DenyMinMaxY.x, DenyMinMaxY.y)), Quaternion.identity);
+            newCloud.transform.parent = LayersGO.transform;
+            newCloud.GetComponent<SpriteRenderer>().sprite = Clouds[Random.Range(0, Clouds.Length)];
+            newCloud.GetComponent<SpriteRenderer>().sortingOrder = -131;
+
+        }
+    }
+}
--- Duaz/Assets/scripts/LocationGeneration.cs
+++ Duaz/Assets/scripts/LocationGeneration.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 public class LocationGeneration : MonoBehaviour
 {
     // Все спрайты используемых декораций
-    [Header("Спрайты декораций")]
+    [Header("Decoration sprites")]
     public Sprite[] TreeDec = new Sprite[5]; // массив спрайтов деревьев
     public Sprite[] GroundRockDec = new Sprite[4], // спрайты камня на поверхности
         UndergroundRockDec = new Sprite[4], // спрайты камня под землей
@@ -17,16 +17,16 @@ public class LocationGeneration : MonoBehaviour
         RubbishDec = new Sprite[5]; // спрайты различного мусора под землей
 
     // параметры управления генерацией объектов
-    [Header("Настройки чанка")]
+    [Header("Chunk settings")]
     public Transform Chunk; // Чанк - центральная точка для генерации декорации в пределах одного чанка
     public Vector2 SizeChunk = new Vector2(3.0f, 0.235f); // Размер одного чанка
     public Vector2Int ChunkGrid = new Vector2Int(3, 3); // количество клеток в одном чанке по х и по у
     Vector2 CellChunk; // координаты одной клетки (результат деления sizeChunk на ChunkGrid)
-    public string AvailableCells; // Все доступные для генерации ячейки
-    public string PossibleCells; // Все оставшиеся доступные ячейки для генерации декораций в пределах одного чанка
+    string AvailableCells; // Все доступные для генерации ячейки
+    string PossibleCells; // Все оставшиеся доступные ячейки для генерации декораций в пределах одного чанка
 
     // параметры управления генерацией объектов
-    [Header("Настройки генерации")]
+    [Header("Generation settings")]
     public float valueGenerateTree; // Шанс генерации дерева (в процентах)
     public float valueGenerateRock, // Шанс генерации наземного камня (в процентах)
         valueGenerateBush, // Шанс генерации куста (в процентах)
@@ -34,10 +34,12 @@ public class LocationGeneration : MonoBehaviour
     int NumberOfChanks; // Количество чанков на всю длину
 
     // параметры управления генерацией объектов
-    [Header("Дополнительно")]
+    [Header("Other")]
     public float LengthLocation = 18; // Длина всей локации
-    public GameObject prefabEmptySprite; // пустая балванка для спрайта
+    public int ZeroLayer; // нулевой слой, начало отсчета для слоев новых объектовs
+    public GameObject prefabEmptySprite; // пустая болванка для спрайта
     public Transform ParentDecoration; // Родительский объект для декораций
+    GameObject[] LayersGO; // группировка декорация по номеру слоя
 
     // Use this for initialization
     void Start ()
@@ -59,11 +61,21 @@ public class LocationGeneration : MonoBehaviour
                 AvailableCells += row + "" + col + ":";
             }
         }
-        ChunkGeneration();
+
+        // Создание пустышек для группировки декорация по номеру слоя
+        LayersGO = new GameObject[ChunkGrid.y];
+        for (int i = 1; i <= ChunkGrid.y; i++)
+        {
+            LayersGO[i - 1] = new GameObject();
+            LayersGO[i - 1].name = "layer";
+            LayersGO[i - 1].tag = "layer";
+            LayersGO[i - 1].transform.parent = ParentDecoration;
+        }
+        ChunkUpGrassGeneration ();
     }
 
-    /*Генерация объектов декорации в пределах одного чанка*/
-    void ChunkGeneration ()
+    /*Генерация объектов декорации в пределах одного чанка на верхней траве над дорогой*/
+    void ChunkUpGrassGeneration ()
     {
         GameObject newDecoration; // новая генерируема декорация
 
@@ -77,17 +89,17 @@ public class LocationGeneration : MonoBehaviour
             {
                 newDecoration = CreateDecoration(TreeDec[Random.Range(0, TreeDec.Length)]); // генерация дерева с случайным спрайтом
                 newDecoration.name = "tree_" + i;
-                newDecoration.transform.parent = ParentDecoration;
+                //newDecoration.transform.parent = ParentDecoration;
             }
 
             // генерация камней
             if (Random.Range(0.0f, 1.0f) < valueGenerateRock)
             {
-                for (int k = 0; k <= Random.Range(1, 2); k++)
+                for (int k = 0; k <= Random.Range(0, 2); k++)
                 {
                     newDecoration = CreateDecoration(GroundRockDec[Random.Range(0, GroundRockDec.Length)]); // генерация наземного камня с случайным спрайтом
                     newDecoration.name = "groundRock_" + i + "_" + k ;
-                    newDecoration.transform.parent = ParentDecoration;
+                    //newDecoration.transform.parent = ParentDecoration;
                 }
             }
 
@@ -95,10 +107,18 @@ public class LocationGeneration : MonoBehaviour
             if (Random.Range(0.0f, 1.0f) < valueGenerateBush)
             {
                 newDecoration = CreateDecoration(BushDec[Random.Range(0, BushDec.Length)]); // генерация куста с случайным спрайтом
-                newDecoration.name = "groundRock_" + i;
-                newDecoration.transform.parent = ParentDecoration;
+                newDecoration.name = "bush_" + i;
+                //newDecoration.transform.parent = ParentDecoration;
             }
-            Chunk.position = new Vector3(Chunk.position.x + SizeChunk.x, Chunk.position.y, Chunk.position.z);
+
+            // генерация травы
+            if (Random.Range(0.0f, 1.0f) < valueGenerateGrass)
+            {
+                newDecoration = CreateDecoration(GrassDec[Random.Range(0, GrassDec.Length)]); // генерация травы с случайным спрайтом
+                newDecoration.name = "grass_" + i;
+                //newDecoration.transform.parent = ParentDecoration;
+            }
+            Chunk.position = new Vector3(Chunk.position.x + SizeChunk.x/2, Chunk.position.y, Chunk.position.z);
         }
     }
 
@@ -120,13 +140,14 @@ public class LocationGeneration : MonoBehaviour
         Vector3 position = Chunk.position + new Vector3(cell.x * CellChunk.x, cell.y * CellChunk.y, 0);
         GameObject newObject = Instantiate(prefabEmptySprite, position, Quaternion.identity);
         newObject.GetComponent<SpriteRenderer>().sprite = sprite;
-        newObject.GetComponent<SpriteRenderer>().sortingOrder = -81 - layer;
+        newObject.GetComponent<SpriteRenderer>().sortingOrder = ZeroLayer - layer;
+        newObject.transform.parent = LayersGO[layer - 1].transform;
 
         // Удаление использованной ячейки из списка оставшихся доступных ячеек
         PossibleCells = "";
         for (int i = 0; i < AllCells.Length; i++)
         {
-            if (i == rndIndexCell || AllCells[i] == "" || AllCells[i].Substring(0, AllCells[1].Length - 1) == cell.x+ ""/*исключает стобец*/) 
+            if (i == rndIndexCell || AllCells[i] == "" || AllCells[i].Substring(0, AllCells[1].Length - 1) == cell.x+ ""/*исключает столбец*/) 
             {
             }
             else
